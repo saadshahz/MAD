888ebbd6122c949575fb4b7a9bd43b232af66196
Saad Shahzad <125588011+saadshahz@users.noreply.github.com>
2025-11-07 22:08:16 +0500

    Calculator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import React from 'react';
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Image, Pressable, StyleSheet, Text, View, TouchableOpacity, Vibration } from 'react-native';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import user from '/assets/images/user.png';
@@ -14,103 +15,122 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handlePress = (value:string) => {
+    Vibration.vibrate(30)
+
+    if (value === "C") {
+      setInput("");
+      setResult("");
+    } else if (value === "=") {
+      if (!input) {
+        setResult("");
+      }
+      else{
+        try {
+          setResult(eval(input).toString());
+        } catch (error) {
+          setResult("Error");
+        }
+      }  
+    } else if(value === "Del"){
+      setInput((prev) => prev.slice(0, -1));
+    }else {
+      setInput((prev) => prev + value);
+    }
+  };
+
+  const buttons = [
+    ["C", "/", "*", "Del"],
+    ["7", "8", "9", "-"],
+    ["4", "5", "6", "+"],
+    ["1", "2", "3", "="],
+    ["0", ".", "%"],
+  ];
+
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <View style={[styles.screen, { backgroundColor: colorScheme === 'dark' ? '#000' : '#f5f5f5' }]}>
-        <View style={styles.card}>
-          {/* Profile Image */}
-          <Image  source={{
-            uri: 'https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=870',
-          }}style={styles.image} />
-
-          {/* Name & Role */}
-          <Text style={[styles.name, { color: colorScheme === 'dark' ? '#fff' : '#222' }]}>
-            Saad Shahzad
-          </Text>
-          <Text style={[styles.role, { color: colorScheme === 'dark' ? '#ccc' : '#666' }]}>
-            Mobile Application Developer
-          </Text>
-
-          {/* Buttons */}
-          <View style={styles.buttonContainer}>
-            <Pressable
-              style={[styles.button, styles.followButton]}
-              onPress={() => console.log('Follow pressed')}
-            >
-              <Text style={styles.followText}>Follow</Text>
-            </Pressable>
-
-            <Pressable
-              style={[styles.button, styles.messageButton]}
-              onPress={() => console.log('Message pressed')}
-            >
-              <Text style={styles.messageText}>Message</Text>
-            </Pressable>
-          </View>
-        </View>
-
-        <StatusBar style="auto" />
+      <View style={styles.container}>
+      {/* Display Section */}
+      <View style={styles.displayContainer}>
+        <Text style={styles.inputText}>{input}</Text>
+        <Text style={styles.resultText}>{result}</Text>
       </View>
+
+      {/* Buttons Section */}
+      <View style={styles.buttonsContainer}>
+        {buttons.map((row, rowIndex) => (
+          <View key={rowIndex} style={styles.row}>
+            {row.map((btn) => (
+              <TouchableOpacity
+                key={btn}
+                style={[
+                  styles.button,
+                  btn === "=" && styles.equalButton,
+                  btn === "C" && styles.clearButton,
+                ]}
+                onPress={() => handlePress(btn)}
+              >
+                <Text style={styles.buttonText}>{btn}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        ))}
+      </View>
+    </View>
     </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
+   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#101010",
+    justifyContent: "flex-end",
   },
-  card: {
-    width: '85%',
-    backgroundColor: '#fff',
-    borderRadius: 20,
-    alignItems: 'center',
-    paddingVertical: 25,
-    paddingHorizontal: 20,
-    shadowColor: '#000',
-    shadowOpacity: 0.15,
-    shadowRadius: 8,
-    shadowOffset: { width: 0, height: 4 },
-    elevation: 5,
+  displayContainer: {
+    padding: 20,
+    marginBottom: 10,
   },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    marginBottom: 15,
+  inputText: {
+    color: "#fff",
+    fontSize: 36,
+    textAlign: "right",
   },
-  name: {
-    fontSize: 20,
-    fontWeight: '700',
+  resultText: {
+    color: "#00FFAA",
+    fontSize: 28,
+    textAlign: "right",
+    marginTop: 10,
   },
-  role: {
-    fontSize: 14,
-    marginBottom: 20,
+  buttonsContainer: {
+    padding: 10,
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    gap: 10,
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
   },
   button: {
+    backgroundColor: "#1E1E1E",
     flex: 1,
-    paddingVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 5,
+    paddingVertical: 20,
     borderRadius: 10,
-    alignItems: 'center',
   },
-  followButton: {
-    backgroundColor: '#007AFF',
+  buttonText: {
+    color: "#fff",
+    fontSize: 24,
   },
-  messageButton: {
-    backgroundColor: '#E5E5E5',
+  equalButton: {
+    backgroundColor: "#00BFFF",
   },
-  followText: {
-    color: '#fff',
-    fontWeight: '600',
-  },
-  messageText: {
-    color: '#000',
-    fontWeight: '600',
+  clearButton: {
+    backgroundColor: "#FF4444",
   },
 });
